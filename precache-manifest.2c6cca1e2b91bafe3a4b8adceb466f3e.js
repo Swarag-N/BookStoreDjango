fc70b16d222c2944f64403db4efdefa30710dba1
Swarag Narayanasetty <46181010+Swarag-N@users.noreply.github.com>
2021-04-28 00:06:34 +0530

    Updates

---
+++ precache-manifest.2c6cca1e2b91bafe3a4b8adceb466f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c94ec471b99f8904b54",
+    "url": "/BookStoreDjango/css/app.384c7bf4.css"
+  },
+  {
+    "revision": "1ba310c44d69c096eca2c41a66a98be0",
+    "url": "/BookStoreDjango/index.html"
+  },
+  {
+    "revision": "f0c5ca4d090d601f095e",
+    "url": "/BookStoreDjango/js/about.3fc2e6af.js"
+  },
+  {
+    "revision": "4c94ec471b99f8904b54",
+    "url": "/BookStoreDjango/js/app.3919d632.js"
+  },
+  {
+    "revision": "bb093ca5cb20ec00a8a2",
+    "url": "/BookStoreDjango/js/chunk-vendors.c675cf18.js"
+  },
+  {
+    "revision": "cb716b5b9cd9a6caa3f1d14a2585cc3f",
+    "url": "/BookStoreDjango/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BookStoreDjango/robots.txt"
+  }
+]);
